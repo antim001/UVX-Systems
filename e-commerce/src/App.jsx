4a4cc0e72a9aeaf9624e424e847a8d436bf504e5
Antim <antimsarker0@gmail.com>
2024-06-10 10:27:00 +0600

    cart page has added

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import NoPage from './pages/NoPage/NoPage';
 import ProductInfo from './pages/ProductInfo/ProductInfo';
 import ScrollTop from './components/scrolltop/ScrollTop';
+import CartPage from './pages/cart/CartPage';
 const App = () => {
   return (
     <div>
@@ -14,6 +15,7 @@ const App = () => {
        <Route path='/' element={<Home></Home>}/>
        <Route path='/*' element={<NoPage></NoPage>}/>
        <Route path='/productinfo' element={<ProductInfo></ProductInfo>}/>
+       <Route path='/cart' element={<CartPage></CartPage>}/>
       </Routes>
     </Router>
     </div>
